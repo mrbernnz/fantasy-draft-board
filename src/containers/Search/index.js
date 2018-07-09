4eb711ef5cf8845b3b57611facf081adc1493fcc
mrbernnz <noel.sagaille@gmail.com>
2018-07-09 10:25:41 -0400

    * Add Search Component.

--- src/containers/Search/index.js
+++ src/containers/Search/index.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export class Search extends Component {
+  state = {
+    term: ''
+  };
+
+  changeHandler = e => {
+    e.preventDefault();
+
+    this.fetchPlayers(this.state.term);
+    this.setState({ term: '' });
+  };
   render() {
     return (
       <form>
-        <input type="search" />
+        <label htmlFor="search">Search Players</label>
+        <input id="search" name="term" type="search" />
       </form>
     );
   }
