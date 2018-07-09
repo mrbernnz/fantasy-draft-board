98324ce614f0c6045019caf661949aee23bff83f
mrbernnz <noel.sagaille@gmail.com>
2018-07-09 10:26:12 -0400

    * Add logger to app.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+if (process.env.NODE_ENV !== 'production') {
+  localStorage.setItem('debug', 'fantasy draft board:*');
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
