4dc1e99811e8af9e670d6010ab48d05772470e52
mrbernnz <noel.sagaille@gmail.com>
2018-07-09 16:20:20 -0400

    * Pass redux store to root App.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
+
+import store from './store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
@@ -8,5 +11,10 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.setItem('debug', 'fantasy draft board:*');
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
