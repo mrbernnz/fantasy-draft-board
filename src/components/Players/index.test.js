bcf1d1f2c679c0abd313adf5bba5c5a2137b34ac
mrbernnz <noel.sagaille@gmail.com>
2018-07-09 22:03:33 -0400

    * Initial test for <PlayersList />.

---
+++ src/components/Players/index.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow, render } from 'enzyme';
+
+import PlayersList from './';
+import { playersData } from '../../mocks/playerData';
+
+describe('<PlayersList />', () => {
+  it('should render zero items', () => {
+    const wrapper = shallow(<PlayersList players={[]} />);
+    expect(wrapper.find('li').length).toBe(0);
+  });
+
+  it('should render some items', () => {
+    const wrapper = render(<PlayersList players={playersData} />);
+    expect(wrapper.find('li').length).toBe(3);
+  });
+});
