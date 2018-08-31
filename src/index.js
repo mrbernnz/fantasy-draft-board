c3ec66a3cf9282e4a628f40473f0c833bd4defc1
mrbernnz <noel.sagaille@gmail.com>
2018-08-31 17:11:53 -0500

    * Modify axios endpoint.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
+import axios from 'axios';
 
+import './index.css';
 import store from './store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+axios.defaults.baseURL = 'http://api.fantasy.nfl.com/v1/players/';
 
 if (process.env.NODE_ENV !== 'production') {
   localStorage.setItem('debug', 'fantasy draft board:*');
