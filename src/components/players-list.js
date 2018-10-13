cc6f65d6c5a104ae22114e211cad3a81c4d76ff0
mrbernnz <noel.sagaille@gmail.com>
2018-10-13 13:22:40 -0500

    * Add conditional element.
      Why:
    
        In case AJAX call return nothing.

--- src/components/players-list.js
+++ src/components/players-list.js
@@ -3,16 +3,20 @@ import React from 'react';
 import Player from './player';
 
 const renderPlayers = players =>
-  players
-    .map(player => (
-      <Player
-        key={player.id}
-        name={player.name}
-        pos={player.position}
-        team={player.teamAbbr}
-      />
-    ))
-    .slice(0, 10);
+  players !== undefined ? (
+    players
+      .map(player => (
+        <Player
+          key={player.id}
+          name={player.name}
+          pos={player.position}
+          team={player.teamAbbr}
+        />
+      ))
+      .slice(0, 10)
+  ) : (
+    <p>No Players List Present</p>
+  );
 
 const PlayersList = ({ players }) => <ul>{renderPlayers(players)}</ul>;
 
